eeaad42d09db22462fa7ae28c68b33157d350fe8
Ivor Wanders <ivor@iwanders.net>
2021-12-31 15:55:42 -0500

    Start of creating the configuration.

--- displaylight/src/lib.rs
+++ displaylight/src/lib.rs
@@ -3,6 +3,110 @@ pub mod rectangle;
 pub mod sampler;
 pub mod zones;
 
+use rectangle::Rectangle;
+use lights;
+
+use std::error::Error;
+use std::{thread, time};
+
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Default)]
+pub struct CaptureSpecification
+{
+    match_width: Option<u32>,
+    match_height: Option<u32>,
+
+    #[serde(default)]
+    x: u32,
+    #[serde(default)]
+    y: u32,
+
+    #[serde(default)]
+    width: u32,
+    #[serde(default)]
+    height: u32,
+
+    #[serde(default)]
+    display: u32,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Default)]
+pub struct Config
+{
+    rate: f32,
+    port: String,
+
+    vertical_depth: u32,
+    horizontal_depth: u32,
+
+    sample_pixel_distance: u32,
+
+    edge_detection_bisect_count: u32,
+
+    limiting_factor: f32,
+
+    capture: Vec<CaptureSpecification>,
+}
+
+pub struct DisplayLight
+{
+    config: Config,
+}
+
+impl DisplayLight
+{
+    const MAX_LEDS: usize = 228;
+
+    fn run(&mut self) -> Result<(), Box<dyn Error>>
+    {
+        let mut grabber = desktop_frame::get_grabber();
+
+        let resolution = grabber.get_resolution();
+
+        println!("Grabber reports resolution of: {:?}", resolution);
+        grabber.prepare_capture(1920, 0, resolution.width - 1920, resolution.height);
+
+        let mut control = lights::Lights::new("/dev/ttyACM0")?;
+        control.set_limit_factor(0.5);
+
+        let mut canvas = [lights::RGB::default(); DisplayLight::MAX_LEDS];
+
+        let mut state: Option<(Rectangle, sampler::Sampler)> = None;
+        loop {
+            let res = grabber.capture_image();
+            if !res {
+                continue;
+            }
+            // Then, grab the image.
+            let img = grabber.get_image();
+
+            // Detect the black borders
+            let borders = border_detection::find_borders(&*img, 4);
+
+            // Border size changed, make a new sampler.
+            if state.is_none() || state.as_ref().unwrap().0 != borders {
+                // With the edges known, we can make the zones.
+                let zones = zones::Zones::make_zones(&borders, 200, 200);
+                // println!("zones: {:?}", zones);
+                assert_eq!(zones.len(), DisplayLight::MAX_LEDS);
+
+                // With the zones known, we can create the sampler.
+                let sampler = sampler::Sampler::make_sampler(&zones, 15);
+                state = Some((borders, sampler));
+            }
+
+            let sampler = &state.as_ref().unwrap().1;
+            // With the sampler, we can now sample and get color values.
+            sampler.sample_into(&*img, &mut canvas);
+
+            control.set_leds(&canvas)?;
+            thread::sleep(time::Duration::from_millis(1000 / 60));
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
--- displaylight/src/main.rs
+++ displaylight/src/main.rs
@@ -1,51 +1,84 @@
-use displaylight::{border_detection, rectangle::Rectangle, sampler, zones};
+use displaylight::{DisplayLight, Config};
 use lights;
 
 use std::error::Error;
-use std::{thread, time};
+
+use std::path::{Path, PathBuf};
+
+extern crate clap;
+use clap::{App, Arg, SubCommand};
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut grabber = desktop_frame::get_grabber();
+    let mut app = App::new("DisplayLight")
+        .about("Controls leds behind the monitor.")
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .help("Override the config, instead of autoselecting based on the OS.."),
+        )
+        .subcommand(
+            SubCommand::with_name("list_ports")
+                .about("List serial ports / com ports and quit.")
+        );
 
-    let resolution = grabber.get_resolution();
+    let matches = app.clone().get_matches();
 
-    println!("Grabber reports resolution of: {:?}", resolution);
-    grabber.prepare_capture(1920, 0, resolution.width - 1920, resolution.height);
-
-    let mut control = lights::Lights::new("/dev/ttyACM0")?;
-    control.set_limit_factor(0.5);
-
-    const MAX_LEDS: usize = 228;
-    let mut canvas = [lights::RGB::default(); MAX_LEDS];
-
-    let mut state: Option<(Rectangle, sampler::Sampler)> = None;
-    loop {
-        let res = grabber.capture_image();
-        if !res {
-            continue;
-        }
-        // Then, grab the image.
-        let img = grabber.get_image();
-
-        // Detect the black borders
-        let borders = border_detection::find_borders(&*img, 4);
-
-        // Border size changed, make a new sampler.
-        if state.is_none() || state.as_ref().unwrap().0 != borders {
-            // With the edges known, we can make the zones.
-            let zones = zones::Zones::make_zones(&borders, 200, 200);
-            // println!("zones: {:?}", zones);
-            assert_eq!(zones.len(), MAX_LEDS);
-
-            // With the zones known, we can create the sampler.
-            let sampler = sampler::Sampler::make_sampler(&zones, 15);
-            state = Some((borders, sampler));
-        }
-
-        let sampler = &state.as_ref().unwrap().1;
-        // With the sampler, we can now sample and get color values.
-        sampler.sample_into(&*img, &mut canvas);
-
-        control.set_leds(&canvas)?;
-        thread::sleep(time::Duration::from_millis(1000 / 60));
+    if let Some(_matches) = matches.subcommand_matches("list_ports") {
+        println!("Ports:\n{:#?}", lights::available_ports()?);
+        return Ok(())
     }
+
+    // Determine what config to load.
+    let mut config_path: Option<PathBuf> = None;
+    if let Some(v_in) = matches.value_of("config") {
+        let path = Path::new(v_in);
+        if path.exists()
+        {
+            config_path = Some(path.to_path_buf())
+        }
+    }
+    else
+    {
+        // No config specified, try to find it in the current directories' config folder.
+        // This is mostly just for development conveniency.
+        let path = PathBuf::from("config").join(format!("{}.yaml", std::env::consts::OS));
+        if path.exists()
+        {
+            config_path = Some(path.to_path_buf());
+        }
+    }
+
+    let mut config: Config = Default::default();
+    if config_path.is_some()
+    {
+        let path = config_path.as_ref().unwrap().to_str().expect("Path is always valid.");
+        println!("Config: {}", path);
+
+        use std::fs::File;
+        use std::io::Read;
+        match File::open(path) {
+            Ok(mut file) => {
+                let mut content = String::new();
+                file.read_to_string(&mut content).expect("Should be able to read the file.");
+                match serde_yaml::from_str(&content)
+                {
+                    Ok(parsed_config) => {config = parsed_config},
+                    Err(failure_message) => {
+                        println!("Something went wrong parsing the configuration file:");
+                        return Err(Box::new(failure_message));
+                    },
+                }
+            }
+            Err(error) => {
+                return Err(Box::new(error));
+            }
+        }
+    }
+    else
+    {
+        println!("No config specified or found.");
+    }
+
+
+    Ok(())
 }
