a5a26106bfd61a98f4a7752fa25d0483f7becdd5
Ivor Wanders <ivor@iwanders.net>
2021-12-31 19:30:21 -0500

    Add start script for linux.

---
+++ displaylight/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+screen -r displaylight > /dev/null
+if [ $? -eq 1 ]; then
+    cd $DIR/
+    screen -dmS "displaylight" cargo run --release
+else
+    echo "already running, should be connecing now"
+fi
