7293f37b76365ec35cbdb26de90822bbefc191e6
Ivor Wanders <ivor@iwanders.net>
2022-07-30 19:01:57 -0400

    Change windows comm port.

--- displaylight/run.sh
+++ displaylight/run.sh
@@ -5,7 +5,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 screen -r displaylight > /dev/null
 if [ $? -eq 1 ]; then
     cd $DIR/
-    screen -dmS "displaylight" cargo run --release
+    screen -dmS "displaylight" ../target/release/displaylight
 else
     echo "already running, should be connecting now"
 fi
